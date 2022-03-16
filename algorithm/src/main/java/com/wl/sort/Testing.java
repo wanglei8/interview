b55e4d7fd06287235bd29c7fdac3ecba859b7070
wl <837804747@qq.com>
2022-03-16 22:19:36 +0800

    初始化

---
+++ algorithm/src/main/java/com/wl/sort/Testing.java
@@ -0,0 +1,12 @@
+package com.wl.sort;
+
+/**
+ * <p></p>
+ *
+ * @author:wanglei
+ */
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
